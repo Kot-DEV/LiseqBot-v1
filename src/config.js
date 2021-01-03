fe999195b756287d8533b9887bacb8461543fa59
PolskiLis19 <63364127+PolskiLis19@users.noreply.github.com>
2021-01-04 00:27:46 +0100

    master

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    // token: 'NzkyNDg2OTA5OTU1ODAxMTEw.X-ea8w.DFlePdYdcHuPUtSkftudMnR4-Wg', // BETA
-    token: 'Nzc1MDY1NjI1NjcyMjg2MjA4.X6g6HQ.iQRSZgTNjtwcXhExlt24VE6aoNM', // FULL
+    // token: 'NzkyNDg2OTA5OTU1ODAxMTEw.X-ea8w.hht66FDNhcWMTD6gWT7hS3lAZ_k', // BETA
+    token: 'Nzc1MDY1NjI1NjcyMjg2MjA4.X6g6HQ.x4hDYf949LwCa4apr0bD6SfjRj0', // FULL
     prefix: "l!", // FULL
     //prefix: "bl!", // BETA
     version: "1.0.5", 
