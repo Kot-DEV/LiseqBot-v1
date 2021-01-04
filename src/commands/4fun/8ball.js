66cd43a16089f28ff61c84a4f2d9b37f339ecaa2
PolskiLis19 <63364127+PolskiLis19@users.noreply.github.com>
2021-01-04 21:57:10 +0100

    master

--- src/commands/4fun/8ball.js
+++ src/commands/4fun/8ball.js
@@ -7,12 +7,24 @@ module.exports.run = (bot, message, args) => {
     if(args[0]) {
         const embed = new discord.MessageEmbed()
         .setTitle('8Ball')
-        var i = getRandomInt(1, 2);
+        var i = getRandomInt(1, 8);
         if(i == 1) {
             embed.setDescription('Tak')
         } else if(i == 2) {
             embed.setDescription('Nie')
-        } 
+        } else if(i == 3) {
+            embed.setDescription('Nie wiem')
+        } else if(i == 4) {
+            embed.setDescription('Nie wiem ale zaraz sie dowiem')
+        } else if(i == 5) {
+            embed.setDescription('Chyba tak')
+        } else if(i == 6) {
+            embed.setDescription('Napewno tak!')
+        } else if(i == 7) {
+        embed.setDescription('Chyba nie')
+        } else if(i == 8) {
+            embed.setDescription('Napewno nie!')
+        }
         message.channel.send(embed);
     } else {
         const embed = new discord.MessageEmbed()
