6f4e83e10ee618f41f6df86565bd11d960d682bc
PolskiLis19 <63364127+PolskiLis19@users.noreply.github.com>
2021-03-01 14:28:58 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
     token: 'Nzc1MDY1NjI1NjcyMjg2MjA4.X6g6HQ.x4hDYf949LwCa4apr0bD6SfjRj0', // FULL
     prefix: "l!", // FULL
     //prefix: "bl!", // BETA
-    version: "1.0.6", 
+    version: "1.0.7", 
     commands: "20",
     mongoPath: "mongodb://localhost:27017"
 }
