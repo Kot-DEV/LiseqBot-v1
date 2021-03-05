15f5ced798fd05fca8f5111cacf076b6dfa9aaa3
PolskiLis19 <63364127+PolskiLis19@users.noreply.github.com>
2021-03-05 14:02:58 +0100

    bugfix v2

--- src/commands/informacyjne/change-log.js
+++ src/commands/informacyjne/change-log.js
@@ -6,7 +6,7 @@ module.exports.run = (bot, message, args) => {
     if(!message.content.startsWith(config.prefix + "change-log")) return;
     const embed = new discord.MessageEmbed()
     .setTitle('Change Log')
-    .setDescription('Nowa komendy:  \n \n - l!8ball \n - l!say \n - l!change-log \n - l!status \n \n Poprawione komendy: \n \n - l!ping \n \n od teraz bot może odpowiadać na polecenia. Np. "uwu", "owo", "bocie!!!" itd.')
+    .setDescription('Nowe komendy:  \n \n - l!8ball \n - l!say \n - l!change-log \n - l!status \n \n Poprawione komendy: \n \n - l!ping \n \n od teraz bot może odpowiadać na polecenia. Np. "uwu", "owo", "bocie!!!" itd.')
     .setColor('#13fc03')
     .setFooter('LiseqBot Nic dodać Nic ująć.')
     message.channel.send(embed)
